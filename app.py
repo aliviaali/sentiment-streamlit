bec88f3a1aee8b8bc0f1a4278e4b4ce7486655e0
aliviaali <125623417+aliviaali@users.noreply.github.com>
2026-01-30 07:41:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import streamlit as st
 st.write("APP BERHASIL DIMUAT")
 import pickle
+nb = pickle.load(open("model_nb.pkl", "rb"))
+svm = pickle.load(open("model_svm.pkl", "rb"))
+tfidf = pickle.load(open("tfidf.pkl", "rb"))
 import re
 
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
 
 
-nb = pickle.load(open("model_nb.pkl", "rb"))
-svm = pickle.load(open("model_svm.pkl", "rb"))
-tfidf = pickle.load(open("tfidf.pkl", "rb"))
+
+
 
